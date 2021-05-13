9629f41bf8856d33815574559f130510d687e563
aorgler <aorgler@iastate.edu>
2021-05-13 13:36:47 -0500

    created repository

---
+++ desktop/src/com/stormwarn/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.stormwarn.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stormwarn.game.StormWarn;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "StormWarn";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new StormWarn(), config);
+	}
+}
